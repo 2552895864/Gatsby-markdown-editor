df408fcbf489c63a8d47730f61c963e7fcbbd942
TadXiao <zlxiao97@yahoo.com>
2020-01-20 15:51:18 +0800

    在master上提交

--- tutorial-part-four/gatsby-config.js
+++ tutorial-part-four/gatsby-config.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 /**
  * Configure your Gatsby site with this file.
  *
@@ -7,23 +8,52 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Pandas Eating Lots`
+    title: 'Gatsby Starter MDX Basic',
+    description:
+      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    author: '@chrisbiscardi',
   },
   plugins: [
-    `gatsby-plugin-emotion`, 
-    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`
+        path: path.join(__dirname, `src`, `images`),
       }
-    }, 
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-remark-images`,
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: `gatsby-plugin-mdx`,
       options: {
-        pathToConfigModule: `src/utils/typography`
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1035,
+              sizeByPixelDensity: true,
+            },
+          }
+        ]
       }
-    }
+    },
+    `gatsby-plugin-feed-mdx`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "standalone",
+        icon: "src/images/gatsby-icon.png",
+        crossOrigin: `use-credentials`,
+      }
+    },
+    'gatsby-plugin-offline',
+    `gatsby-plugin-react-helmet`
   ]
 }
--- tutorial-part-four/src/components/layout.js
+++ tutorial-part-four/src/components/layout.js
@@ -1,48 +1,41 @@
 import React from 'react'
-import { css } from '@emotion/core'
-import { Link, useStaticQuery, graphql } from 'gatsby'
-import { rhythm } from '../utils/typography'
-export default ({children})=>{
-    const data = useStaticQuery(
-        graphql`
-            query {
-                site {
-                    siteMetadata {
-                        title
-                    }
-                }
-            }
-        `
-    );
-    return (
-        <div 
-            css={css`
-                margin: 0 auto;
-                max-width: 700px;
-                padding: ${rhythm(2)};
-                padding-top: ${rhythm(1.5)};
-            `}
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './header'
+import './layout.css'
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
         >
-            <Link to="/">
-                <h3 
-                    css={css`
-                        font-style: normal;
-                        display: inline-block;
-                        margin-bottom: ${rhythm(2)};
-                    `}
-                >
-                    {data.site.siteMetadata.title}
-                </h3>
-            </Link>
-            <Link 
-                to="/about/"
-                css={css`
-                    float: right;
-                `}
-            >
-                About
-            </Link>
-            {children}
+          {children}
         </div>
-    )
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
 }
+
+export default Layout
